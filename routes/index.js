b11223e701ad2af1d4c7e140f00a513bd3ab7542
Vivekananda Mayura <GBVIVEKANANM@tetrapak.com>
2023-03-06 21:36:47 +0000

    Fixed Fetch

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 router.get('/', async (req, res) =>
 {
